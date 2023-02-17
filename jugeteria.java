67e5f89a6d908ff3d08044f7f991d94f776961ec
DieGeoS <ds7050750@gmail.com>
2023-02-16 20:33:01 -0600

    jugeteria

---
+++ jugeteria.java
@@ -0,0 +1,13 @@
+class Jugeteria extends Producto{
+    materia;
+    rango_edad;
+    categoria;
+    niño/niña;
+    constructor(materal,rg,categoria,n/n)
+    super(){
+    this.material = material;
+    this.rango_edad = rg;
+    this.categoria = categoria;
+    this.niño/niña = n/n;
+    }
+}
