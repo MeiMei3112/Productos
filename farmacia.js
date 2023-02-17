db0cb891e16317447973c296844652ac660d0303
NeftaSg <sneftaly49@gmail.com>
2023-02-16 22:21:38 -0600

    nuevos cambios

--- farmacia.js
+++ farmacia.js
@@ -1,16 +1,13 @@
 class Farmacia extends Producto{
- contenido;
- tipo_de_administracion;
- solo_con_receta;
-tipo_de_pureza;
-
-constructor (contenido, tipo_de_administrador, solo_con_receta, tipo_de_pureza){
-
-    super(contenido, solo_con_receta){
+    contenido;
+    tipo_de_administracion;
+    solo_con_receta;
+    tipo_de_pureza;
+    constructor (cont, tipoad, receta, tipopurez)
+    super(){
     this.Contenido = cont;
     this.Tipo_de_administrador = tipoad;
     this.Solo_con_receta = receta;
     this.Tipo_de_pureza = tipopurez;
-           
+    }
 }
-       
