48cacab4f3545186aeff038e7c4d083a6ddbbd2d
SONIAJAZ <soniatrujillocruz@gmail.com>
2023-02-20 10:30:09 -0600

    cambio2 jaz

---
+++ Admin.js
@@ -0,0 +1,36 @@
+class Admin {
+    id;
+    Nombre;
+    Apellidos;
+    #correo;
+    #password;
+    constructor(name,lastname){
+        this.Nombre =name;
+        this.id =  this.crearId();
+        this.Apellidos=lastname();
+        this.#correo=this.actualizarCorreo();
+        this.#password=this.actualizarPassword();
+    }
+
+   /**
+    * 
+    * @param {string} correo 
+    */ 
+   actualizarCorreo = function(correo){
+    this.#correo = correo;
+}
+/**
+    * 
+    * @param {string} password
+    */ 
+   actualizarPassword = function(password){
+    this.#password = password;
+}
+   /**
+     * 
+     * @returns {Int}
+     */
+   crearId= function(){
+    let name =  this.Nombre;
+    return  name.charAt(0) + Math.floor(Math.random() * 1000);
+}
